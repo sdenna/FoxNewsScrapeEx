ebb005c6d8a4faae05fee72cfae99fec86230f67
sdenna17 <49918103+sdenna17@users.noreply.github.com>
2020-01-23 08:15:34 -0600

    Added a menu to represent the options that were previously on the buttons.  When the user selects one a toast pops up (just to signify that the menu responded)

--- app/src/main/java/com/example/simplescrapeex/MainActivity.java
+++ app/src/main/java/com/example/simplescrapeex/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -35,6 +39,31 @@ public class MainActivity extends AppCompatActivity {
         buttonFoxOpinion = (Button)findViewById(R.id.buttonOpinionFox);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(),"Item 1 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(),"Item 2 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(),"Item 3 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 
     public void scrapeFoxMain(View v) {
